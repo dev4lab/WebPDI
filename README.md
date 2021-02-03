3e7c72a4812f1d67a727c3fe4e4b866321bf01ab
NataliaCarvalho03 <carvalho.natalia03@gmail.com>
2021-02-03 17:13:43 -0300

    Processamento de brilho e contraste e adicao das docs

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # WebPDI
-A simple Web App to change light and contrast of images.
+Um simples app web para processamento de imagens utilizando FastApi e OpenCV.
+
+# How to run it
+
+1. Instale as dependências:
+
+```
+pip install -r requirements.txt
+```
+
+2. Rode o servidor:
+
+```
+uvicorn main:app --reload
+```
+
+3. Abra seu navegador e acesse ```http://localhost:8000/```. Você verá esta janela:
+
+![](doc/images/home.png)
+
+Clique em "Escolher arquivo" e escolha uma imagem <b>em tons de cinza<b> para fazer upload. Depois, clique em "Enviar".
+
+Você será redirecionado para esta página:
+
+![](doc/images/item.png)
+
+
+4. Digite um número inteiro nos campos de texto Brilho e Contraste. É interessante que o número para brilho seja entre 0 e 100 e o de contraste esteja entre 2 e 5 (Acima disso, a imagem vai ficar estranha).
+
+Eu digitei os numeros 50 para Brilho e 2 para Contraste.
+
+5. Clique em "Submit" e a imagem modificada aparecerá ao lado direito, como na imagem abaixo:
+
+![](doc/images/result.png)
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from starlette.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+import cv2 as cv
+import numpy as np
+
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -24,4 +27,21 @@ async def create_upload_file(image: UploadFile = File(...)):
 
 @app.post('/edit/')
 async def edit_image(request: Request):
+    return templates.TemplateResponse('item.html', {'request': request})
+
+@app.get('/change/')
+async def change_image(request: Request, contraste: int, brilho: int):
+
+    image = cv.imread('static/images/actual.png', 0)
+    
+    im = np.double(image.copy())
+    im = im * contraste + brilho #O valor de todos os pixels está sendo multiplicado por l
+
+    #Garantindo que os valores de pixel não vão passar de 255 e nem estar abaixo de 0
+    im[im > 255] = 255
+    im[im < 0] = 0
+
+    im = np.uint8(im)
+
+    cv.imwrite('static/images/changed.png', im)
     return templates.TemplateResponse('item.html', {'request': request})
